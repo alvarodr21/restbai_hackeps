d16b6a7fdcf8c5f7c6a16e142e573112c356f2a7
alvarodr21 <alvarodr21@gmail.com>
2023-11-25 21:49:39 +0100

    text input

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, { useState } from 'react';
+import {Button, StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  const handleInputChange = (text) => {
+    setInputText(text);
+  };
+
+  const handleButtonPress = () => {
+    setDisplayText(inputText);
+    // You can perform other actions here based on the entered text if needed
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-        <Button title={"Hello"}></Button>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Type here..."
+        onChangeText={handleInputChange}
+        value={inputText}
+      />
+
+      <Button title="Press me" onPress={handleButtonPress} />
+
+      <Text>Entered text: {displayText}</Text>
     </View>
   );
 }
